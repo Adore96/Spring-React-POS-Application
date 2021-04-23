875d4754aee3e4cf163535c7609e727b934f6f4c
Kalhara Perera <adorekasun@gmail.com>
2021-04-23 17:00:35 +0530

    Bill Component added

---
+++ sample-app/src/components/billComponents/ViewSingleBillComponent.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ViewSingleBillComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+ViewSingleBillComponent.propTypes = {};
+
+export default ViewSingleBillComponent;
