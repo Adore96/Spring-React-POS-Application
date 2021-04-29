acbc29c64768b4d5bade084ccba720494e19efbf
kasun Perera <adorekasun@gmail.com>
2021-04-29 14:49:09 +0530

    Bill Service Update

--- ReactBackend/src/main/java/com/adore96/ReactBackEnd/service/BillService.java
+++ ReactBackend/src/main/java/com/adore96/ReactBackEnd/service/BillService.java
@@ -5,5 +5,15 @@ Project ReactBackEnd
 On 4/28/2021
 */
 
+import com.adore96.ReactBackEnd.util.TimeStampGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BillService {
+
+    @Autowired
+    TimeStampGenerator timeStampGenerator;
+
+
 }
